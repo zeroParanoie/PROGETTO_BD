6ce696ec35e1f7c5dc3c01e5ba93fcae7e44bf21
zeroParanoie <andrea.tsilogiannis@gmail.com>
2024-01-27 19:11:26 +0100

    added population, needs data

---
+++ sql/eventoSpedizioni.sql
@@ -0,0 +1,9 @@
+set global event_scheduler = on;
+CREATE event IF NOT EXISTS spedizione_ordini
+	on schedule
+    every 2 day
+		on completion preserve
+	comment 'shipping orders'
+    do
+		UPDATE ordine SET stato = 'in consegna'
+
