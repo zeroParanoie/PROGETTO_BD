c40fb2a5b1dfb64a2e6583af8d1cea6e1850ad1c
zeroParanoie <andrea.tsilogiannis@gmail.com>
2024-02-05 16:17:36 +0100

    instance script update

--- sql/eventoOrdini.sql
+++
@@ -1,8 +0,0 @@
-set global event_scheduler = on;
-CREATE event IF NOT EXISTS cleanup_ordini
-	on schedule
-	every 2 day
-		on completion preserve
-	comment 'remove shipped orders'
-	do
-		delete from ordine where stato = 'consegnato';
--- sql/eventoSpedizioni.sql
+++
@@ -1,9 +0,0 @@
-set global event_scheduler = on;
-CREATE event IF NOT EXISTS spedizione_ordini
-	on schedule
-    every 2 day
-		on completion preserve
-	comment 'shipping orders'
-    do
-		UPDATE ordine SET stato = 'in consegna' WHERE stato = 'finalizzato';
-
